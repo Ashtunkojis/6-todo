be23dcb0f1c3923e22c3969be6e6b17acec08577
Andrius K <andkubilius@gmail.com>
2021-02-04 13:49:36 +0200

    comment

--- js/components/Todo.js
+++ js/components/Todo.js
@@ -28,8 +28,8 @@ class Todo {
 
         const filtered = [];
 
-        //  let filtered1 = filtered.filter(i => i !== index).push(this.list[i]);
-        //  this.list = filtered1;
+        // let filtered1 = filtered.filter(i !== index).push(this.list[i]);
+        // this.list = filtered1;
 
         for (let i = 0; i < this.list.length; i++) {
             if (i !== index) {
